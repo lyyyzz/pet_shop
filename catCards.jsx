a013aeb7cfef8497df52ea148496ab2496842574
lyyyzz <156385187+lyyyzz@users.noreply.github.com>
2024-08-25 06:39:09 +0400

    Update catCards.jsx

--- catCards.jsx
+++ catCards.jsx
@@ -1,17 +1,20 @@
-import burmilla from './burmilla.PNG'
-import korat from './korat.PNG'
-import bombay from './bombay.PNG'
+import burmilla from './pictures/burmilla.PNG'
+import korat from './pictures/korat.PNG'
+import bombay from './pictures/bombay.PNG'
+import birman from './pictures/birman.PNG'
 
 const images = {
     burmilla: burmilla,
     korat:korat,
     bombay:bombay,
+    birman:birman,
   };
 
   const catBreeds = [
-    { id: 6, name: 'burmilla', description:'black', price:500, isPopular: false, stock:5},
-    { id: 7, name: 'korat', description: 'korat', price: 400, isPopular: false, stock: 7 },
-    { id: 8, name: 'bombay', description: 'bombay', price: 700, isPopular: false, stock: 2 },
+    { id: 5, name: 'burmilla', description:'Sleek, playful, and affectionate, with a stunning silver-grey coat and golden eyes.', price:500, isPopular: false, stock:5},
+    { id: 6, name: 'korat', description: 'Silver-blue coat, heart-shaped face, and piercing green eyes, with a playful, curious, and loyal personality.', price: 400, isPopular: false, stock: 7 },
+    { id: 7, name: 'bombay', description: 'Glossy black coat, golden or copper eyes, and a muscular build, with a playful, outgoing, and affectionate personality', price: 700, isPopular: false, stock: 2 },
+    { id: 12, name: 'birman', description: 'The Birman is a calm, gentle, and affectionate cat with a distinctive pointed coat pattern and bright blue eyes.', price: 600, isPopular: false, stock: 2 },
   ]
 
   export { catBreeds, images };
@@ -21,12 +24,13 @@ const images = {
       <div className="">
         <div className="card-wrapper">
           <img className="pictures" src={images[breed.name]} alt={breed.name} />
-          <div className="pet-texts">
+          <div className="pet-details">
           <p>{breed.description}</p>
           <p>Price: ${breed.price}</p>
           <p>Is Popular: {breed.isPopular ? 'Yes' : 'No'}</p>
           <p>Stock: {breed.stock}</p>
           </div>
+          <h2 className="breed-name">{breed.name}</h2>
         </div>
       </div>
     );
